2688b6a9865b24980e1cdee48d4da1d3df5dddb4
Pavan Kumar Singh <pavanrnri1818@gmail.com>
2025-01-19 11:36:40 +0530

    Created nested fromGroup
    Successfully created formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,32 @@ baseURL = 'http://localhost:8080/login'
   title = 'reactive-form';
   profileForm = new FormGroup({
     name : new FormControl(''),
-    email : new FormControl('')
+    email : new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    })
   })
  constructor(private service: FormsServicesService) {}
  ngOnInit(): void {
    
  }
  onSubmit(): void {
-  this.service.submitForm(this.baseURL,this.profileForm.value).subscribe((res:any)=>{
-    console.log("res______________________",res)
-  })
+  // this.service.submitForm(this.baseURL,this.profileForm.value).subscribe((res:any)=>{
+  //   console.log("res______________________",res)
+  // })
   console.log("profile data",this.profileForm.value)
+ }
+
+ updateProfile(): any {
+  this.profileForm.patchValue({
+    name: 'Nancy',
+    address: {
+      street: '123 Drew Street',
+    },
+  })
+  console.log("form data",this.profileForm.value)
  }
 }
